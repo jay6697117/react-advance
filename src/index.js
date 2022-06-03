927052dc894eff460cfe42b083e2a9738da0b5d6
steve zhang <871506263@qq.com>
2022-06-03 20:58:32 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import rux from 'ruxjs'; // 暂时注释
-console.log('rux :>> ', rux);
+// import rux from 'ruxjs'; // 暂时注释
+// console.log('rux :', rux);
 import './assets/styles/common.scss';
 
 ReactDOM.render(React.createElement(App), document.getElementById('app'));
